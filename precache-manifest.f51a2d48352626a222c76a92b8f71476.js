e8d7b96ea1b742e9ef867db295d0a351ef7c6c6f
LAPTOP-C403633C\Dika Handika <dika.csui@gmail.com>
2019-09-22 15:53:05 +0700

    deploy

---
+++ precache-manifest.f51a2d48352626a222c76a92b8f71476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/currency-exchange/robots.txt"
+  },
+  {
+    "revision": "22480a61890427c974cf",
+    "url": "/currency-exchange/js/chunk-vendors.e856292f.js"
+  },
+  {
+    "revision": "62e546e00f5c915f71b0",
+    "url": "/currency-exchange/js/app.027808a4.js"
+  },
+  {
+    "revision": "5e43f9c8bda50a5791506796f6893b54",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "d5b711e94d3cc893ef6f500eb1025176",
+    "url": "/currency-exchange/img/trash.d5b711e9.svg"
+  },
+  {
+    "revision": "62e546e00f5c915f71b0",
+    "url": "/currency-exchange/css/app.7b4bd8cf.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/currency-exchange/precache-manifest.f51a2d48352626a222c76a92b8f71476.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "foreign-exchange-currency"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
